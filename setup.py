1fc7fc63d1141840a44f94b2bce663f18ae2f05e
Ryan T. Hamilton <astrobokonon@gmail.com>
2018-05-18 15:56:04 -0700

    Fixing the license classifier

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ setup(
         'Topic :: Software Development :: Build Tools',
 
         # Pick your license as you wish
-        'License :: OSI Approved :: MPL 2.0 License',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
