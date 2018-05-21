146ce16edecf2f3bd05972e96ebf0c336f2827c6
Ryan T. Hamilton <astrobokonon@gmail.com>
2018-05-21 13:59:24 -0700

    Trying to figure out package_data includes

--- setup.py
+++ setup.py
@@ -158,8 +158,7 @@ setup(
     #
     # If using Python 2.6 or earlier, then these have to be included in
     # MANIFEST.in as well.
-    package_data={
-    },
+    package_data={'ligmos': ['schemas/*.xsd']},
 
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
