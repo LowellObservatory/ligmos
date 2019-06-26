c0ca55afc3688ac1c75ef70d6a97bdf8824e09ac
Ryan T. Hamilton <astrobokonon@gmail.com>
2019-06-26 16:44:33 -0700

    Improve config warning

--- ligmos/workers/confUtils.py
+++ ligmos/workers/confUtils.py
@@ -121,7 +121,7 @@ def assignConf(conf, obj, backfill=False, debug=False):
         except KeyError:
             # This means that
             if debug is True:
-                print("Improper config; missing configuration key %s" % (key))
+                print("Missing expected configuration key %s" % (key))
             # Just set it to None and move on with our lives
             setattr(classy, key, None)
 
