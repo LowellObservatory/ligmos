2660cdd3e3e6e2bdbe4cc0beff0ce2b25d0e226d
Ryan T. Hamilton <astrobokonon@gmail.com>
2020-02-12 11:56:55 -0700

    Skeleton for a custom listener example

---
+++ examples/customListener.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  Created on 12 Feb 2020
+#
+#  @author: rhamilton
+
+"""One line description of module.
+
+Further description.
+"""
+
+from __future__ import division, print_function, absolute_import
+
+import ligmos
+
+
+if __name__ == "__main__":
+    pass
