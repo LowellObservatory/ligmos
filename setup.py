3d6d8e518e2a6dd2e0ac23bfa4c38a1e2ff37043
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-07-01 11:52:38 -0700

    PEP8

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ See:
 https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
+from os import path
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+
 
 here = path.abspath(path.dirname(__file__))
 
