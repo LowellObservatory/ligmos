d75656fe2807e85c7c72ff8a6f5a4802a596eeae
Ryan T. Hamilton <astrobokonon@gmail.com>
2021-08-17 12:47:21 -0700

    Move xmlschema stuff out to module

--- ligmos/utils/amq.py
+++ ligmos/utils/amq.py
@@ -16,14 +16,11 @@ Further description.
 from __future__ import division, print_function, absolute_import
 
 import secrets
-from os.path import basename
 
 import stomp
-import xmlschema as xmls
-import pkg_resources as pkgr
 
 from . import classes
-from . import amq_listeners as amql
+from . import amqListeners as amql
 
 
 class amqHelper():
@@ -412,119 +409,6 @@ def checkTopic(tpc):
         topic = tpc
 
     return topic
-
-
-def checkSchema(topicname):
-    """
-    """
-    # Put together the expected schema name
-    sn = 'schemas/' + topicname + '.xsd'
-    try:
-        # Define the schema we'll use to convert datatypes. If it doesn't
-        #   exist, catch the exception and return 'None' to show that
-        #   the schema didn't exist where it was expected
-        sf = pkgr.resource_filename('ligmos', sn)
-        schema = xmls.XMLSchema(sf)
-        return schema
-    except xmls.exceptions.XMLSchemaException as err:
-        print("Problem with schema for topic %s!" % (topicname))
-        print(str(err))
-        return None
-    except OSError as err:
-        print("Schema %s not found!" % (sf))
-        return None
-
-
-def checkSample(topicname):
-    """
-    """
-    # Put together the expected schema name
-    sn = 'schemas/xmlsamples/' + topicname + '.xml'
-    try:
-        # Define the schema we'll use to convert datatypes. If it doesn't
-        #   exist, catch the exception and return 'None' to show that
-        #   the schema didn't exist where it was expected
-        sf = pkgr.resource_filename('ligmos', sn)
-        xmlstr = ""
-        with open(sf, 'r') as f:
-            xmlstr = f.read()
-
-        return xmlstr
-    except xmls.exceptions.XMLSchemaException:
-        print("Sample for topic %s not found!" % (topicname))
-        return ""
-    except (IOError, OSError):
-        print("Sample for topic %s not found!" % (topicname))
-        return ""
-
-
-def schemaDicter():
-    """
-    Grab all of the schemas in the package directory and return
-    a dict organized by topic name.
-    """
-    sdict = {}
-
-    # Get the list of everything in the schema repo
-    allschemas = pkgr.resource_listdir('ligmos', 'schemas')
-
-    for tsch in allschemas:
-        spath = "%s/%s" % ('schemas', tsch)
-        schname = basename(tsch)
-
-        # Strip the file extension off of it!
-        schname = schname[:-4]
-
-        if pkgr.resource_isdir('ligmos', spath):
-            print("%s is a directory! Skipping it." % (tsch))
-        else:
-            print("%s is a potential schema! Looking at it." % (schname))
-
-            # Try to peel off a version tag, if there is one. It needs to be a
-            #   very specific format!
-            # broker.topic.name.vX-Y-Z.xsd
-            schparts = schname.split(".")
-            # if it starts with "v" AND also has underscores, it's probably
-            #   a tag.  It's dumb but it works for now. Could be improved.
-            if schparts[-1].startswith("v"):
-                if "_" in schparts[-1]:
-                    vtag = schparts[-1]
-                    origtag = schname[:-len(vtag)-1]
-                    # Some more transformations to clean it up to what should
-                    #   be in the packets themselves
-                    vtag = vtag.replace("_", ".")
-            else:
-                origtag = schname
-                vtag = None
-
-            try:
-                # Define the schema we'll use to convert datatypes
-                sf = pkgr.resource_filename('ligmos', spath)
-                schema = xmls.XMLSchema(sf)
-                # Deal with our multi-version case
-                if vtag is not None:
-                    print("Storing %s %s" % (origtag, vtag))
-                    versionedSchema = {vtag: schema}
-                    # Make sure that if we have a version tag, we don't
-                    #   clobber something else already stored
-                    if origtag in sdict.keys():
-                        existingEntry = sdict[origtag]
-                        existingEntry.update(versionedSchema)
-                        sdict.update({origtag: existingEntry})
-                    else:
-                        sdict.update({origtag: versionedSchema})
-                else:
-                    sdict.update({origtag: schema})
-            except xmls.exceptions.XMLSchemaException as err:
-                print("xmlschema error!")
-                print(str(err))
-                print("Schema for topic %s has been abandoned!" % (schname))
-            except xmls.etree.ParseError as err:
-                print("xmlschema error!")
-                print(str(err))
-                print("Schema for topic %s has been abandoned!" % (schname))
-
-    return sdict
 
 
 def disconnectAll(amqbrokers):
